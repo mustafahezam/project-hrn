0fd89631670f65f395e396a19bf04465dc67ae8a
mustafahezam <mustafahezam1@gmail.com>
2021-10-24 15:53:45 +0300

    the day two first commit

---
+++ init.php
@@ -0,0 +1,14 @@
+<?php
+
+   $App  = 'includes/App/';
+   $func = 'incluses/function.php';
+
+   include $App . 'head.php';
+   include $App . 'aside.php';
+   include $App . 'asideHidden.php';
+   include $App . 'asideMobile.php';
+   include $App . 'chat.php';
+   include $App . 'nav.php';
+   include $App . 'navMobile.php';
+
+?>
